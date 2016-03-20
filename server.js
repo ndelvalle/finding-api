ea938ee116772cbd05c4e49d88008ffb29b3f8fd
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-20 23:02:54 +0100

    Added helmet dependency

--- server.js
+++ server.js
@@ -6,25 +6,24 @@ var port   = config.PORT;
 
 var express    = require('express');
 var app        = express();
+var mongoose   = require('mongoose');
+var helmet     = require('helmet');
 var bodyParser = require('body-parser');
 var morgan     = require('morgan');
-
-var mongoose   = require('mongoose');
+var Promise    = require('bluebird');
 
 var routes      = require('./routes');
 var middlewares = require('./middlewares');
 
-var Promise = require('bluebird');
 Promise.promisifyAll(mongoose);
 mongoose.connect(config.MONGODB_URL);
 
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use(middlewares.cors);
-
 app.use(morgan('tiny'));
 
+app.use(middlewares.cors);
 app.use('/', routes);
 
 app.listen(port);
