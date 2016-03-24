8adaee44f7d14f4ec4a7f1476626c2b1a596d8d0
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-24 23:44:42 +0100

    Added post and put routes for missing model

--- routes.js
+++ routes.js
@@ -6,12 +6,16 @@ var controllers = require('./controllers');
 
 router.get('/', function(req, res) {
   res.json({
-    message: 'Welcome to Missing Person API!'
+    message: 'Welcome to Missing People API!'
   });
 });
 
 router.route('/missing')
-  .get(controllers.missing.find.bind(controllers.missing));
+.get(controllers.missing.find.bind(controllers.missing))
+.post(controllers.missing.create.bind(controllers.missing));
 
+router.route('/missing/:id')
+  .get(controllers.missing.findOne.bind(controllers.missing))
+  .put(controllers.missing.update.bind(controllers.missing));
 
 module.exports = router;
