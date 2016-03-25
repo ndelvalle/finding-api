c0ab8e134039a71c3b5578c4420afe364485f57c
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-25 23:49:39 +0100

    Created user schema

---
+++ schemas/user-schema.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
