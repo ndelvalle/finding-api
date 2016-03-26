d313fd8d73c65d3febdd8f86a953e0ca39530121
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-26 02:32:12 +0100

    Added EXPIRATION and KEY params to JWT config

--- config/config.js
+++ config/config.js
@@ -3,5 +3,9 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/missing',
-  ENV: process.env.NODE_ENV || 'development'
+  ENV: process.env.NODE_ENV || 'development',
+  JWT: {
+    KEY: process.env.JWT_KEY || '92601aa638620ae31418',
+    EXPIRATION: process.env.JWT_EXPIRATION ||  86400 // 1 day
+  }
 };
