718547036196fa2040107bd52bc03f1d0f7917cd
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-26 02:31:03 +0100

    Used bluebird promisifyAll method on jsonwebtoken and bcrypt libs

--- server.js
+++ server.js
@@ -4,18 +4,24 @@
 var config = require('./config/config');
 var port   = config.PORT;
 
-var express    = require('express');
-var app        = express();
-var mongoose   = require('mongoose');
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var Promise  = require('bluebird');
+var jwt      = require('jsonwebtoken');
+var bcrypt   = require('bcrypt');
+
 var helmet     = require('helmet');
 var bodyParser = require('body-parser');
 var morgan     = require('morgan');
-var Promise    = require('bluebird');
 
 var routes      = require('./routes');
 var middlewares = require('./middlewares');
 
 Promise.promisifyAll(mongoose);
+Promise.promisifyAll(jwt);
+Promise.promisifyAll(bcrypt);
+
 mongoose.connect(config.MONGODB_URL);
 
 app.use(helmet());
