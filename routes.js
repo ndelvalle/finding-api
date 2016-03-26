c0c8595554e33b2fdc8b970cc7cfbdf293ea6ba1
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-26 02:37:38 +0100

    Added private routes

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@
 var router = require('express').Router();
 
 var controllers = require('./controllers');
+var middlewares = require('./middlewares');
+
+// public routes
 
 router.get('/', function(req, res) {
   res.json({
@@ -10,12 +13,24 @@ router.get('/', function(req, res) {
   });
 });
 
+router.route('/authenticate')
+  .post(controllers.user.authenticate.bind(controllers.user));
+
 router.route('/missing')
-.get(controllers.missing.find.bind(controllers.missing))
-.post(controllers.missing.create.bind(controllers.missing));
+  .get(controllers.missing.find.bind(controllers.missing));
 
 router.route('/missing/:id')
-  .get(controllers.missing.findOne.bind(controllers.missing))
+  .get(controllers.missing.findOne.bind(controllers.missing));
+
+
+// private routes, need to be authenticated to acces
+
+router.use(middlewares.auth);
+
+router.route('/missing/:id')
   .put(controllers.missing.update.bind(controllers.missing));
+
+router.route('/missing')
+  .post(controllers.missing.create.bind(controllers.missing));
 
 module.exports = router;
