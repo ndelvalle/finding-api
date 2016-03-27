3a53caa7cd944b63ce95cbfe6b78ce7dd4cb38de
ndelvalle <nicolas.delvalle@gmail.com>
2016-03-27 18:40:22 +0200

    Added promisify all library

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ var port   = config.PORT;
 var express  = require('express');
 var app      = express();
 var mongoose = require('mongoose');
-var Promise  = require('bluebird');
-var jwt      = require('jsonwebtoken');
-var bcrypt   = require('bcrypt');
 
 var helmet     = require('helmet');
 var bodyParser = require('body-parser');
@@ -18,9 +15,7 @@ var morgan     = require('morgan');
 var routes      = require('./routes');
 var middlewares = require('./middlewares');
 
-Promise.promisifyAll(mongoose);
-Promise.promisifyAll(jwt);
-Promise.promisifyAll(bcrypt);
+require('./libraries/promisify-all')(['mongoose', 'jsonwebtoken', 'bcrypt']);
 
 mongoose.connect(config.MONGODB_URL);
 
