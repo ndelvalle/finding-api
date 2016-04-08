950aa9a09e158718897bbd1326299197f4d08766
ndelvalle <nicolas.delvalle@gmail.com>
2016-04-08 02:39:34 +0200

    Used arrow function

--- libraries/promisify-all.js
+++ libraries/promisify-all.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var _  = require('lodash');
-var Promise  = require('bluebird');
+const _ = require('lodash');
+const Promise = require('bluebird');
 
-module.exports = function (modules) {
-  _.each(modules, function(module) {
+module.exports = modules => {
+  _.each(modules, module => {
     Promise.promisifyAll(require(module));
   });
 };
