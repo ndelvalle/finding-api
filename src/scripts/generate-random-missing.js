ba106cd6504fb1379f7d2cd1a7d38ab1d52aa3da
ndelvalle <nicolas.delvalle@gmail.com>
2016-04-12 01:05:15 +0200

    Migrate script to es6

--- src/scripts/generate-random-missing.js
+++ src/scripts/generate-random-missing.js
@@ -1,23 +1,19 @@
-'use strict';
+import config from '../config/config';
+import Missing from '../models/missing-model';
 
-var config = require('../config/config');
-
-var mongoose = require('mongoose');
-var Promise  = require('bluebird');
-var Faker    = require('Faker');
-
-var Missing = require('../models/missing-model');
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
+import Faker from 'Faker';
 
 Promise.promisifyAll(mongoose);
 mongoose.connect(config.MONGODB_URL);
-mongoose.connect('mongodb://heroku_q0jc3w1d:np4n527c13jdsqd1g2qmnuncfl@ds011439.mlab.com:11439/heroku_q0jc3w1d');
 
 // amount of random data to generate
-var AMOUNT = 20;
+const AMOUNT = 20;
 
 function generateGeo() {
   return {
-    loc: [ -110.8571443, 32.4586858 ],
+    loc: [-110.8571443, 32.4586858],
     name: 'AlleyNYC',
     line1: '500 7th Ave',
     line2: 'Floor 17A',
@@ -29,53 +25,50 @@ function generateGeo() {
     state: 'New York',
     countryCode: 'US',
     country: 'United States',
-    timezone: 'America/New_York'
+    timezone: 'America/New_York',
   };
 }
 
 function generatePerson(geo) {
   return {
-    name: {
-      first: Faker.Name.firstName(),
-      last: Faker.Name.lastName()
-    },
+    name: `${Faker.Name.firstName()} ${Faker.Name.lastName()}`,
     age: Math.floor(Math.random() * (80 - 18 + 1) + 18),
     gender: ['M', 'F'][Math.floor(Math.random() * ['M', 'F'].length)],
-    geo: geo
+    geo,
   };
 }
 
 function generateMissing() {
-  var geo = generateGeo();
-  var person = generatePerson(geo);
+  const geo = generateGeo();
+  const person = generatePerson(geo);
 
   return Missing.create({
-    person: person,
+    person,
     lastSeen: new Date(),
     photos: [{
-      url: Faker.Image.avatar()
+      url: Faker.Image.avatar(),
     }, {
-      url: Faker.Image.avatar()
+      url: Faker.Image.avatar(),
     }],
     description: {
       clothing: Faker.Lorem.sentence(),
       appearance: Faker.Lorem.sentence(),
-      relevantData: Faker.Lorem.sentence()
-    }
+      relevantData: Faker.Lorem.sentence(),
+    },
   });
 }
 
-var promiseQueue = [];
-for (var i = 0; i < AMOUNT; i++) {
+const promiseQueue = [];
+for (let i = 0; i < AMOUNT; i++) {
   promiseQueue.push(generateMissing());
 }
 
 Promise.all(promiseQueue)
-  .then(function(ppl) {
-    console.log(ppl.length + ' people has been created');
-    process.exit();
-  })
-  .catch(function(err) {
-    console.log(err);
-    process.exit(1);
-  });
+.then(ppl => {
+  console.log(`${ppl.length} people has been created`); // eslint-disable-line no-console
+  process.exit();
+})
+.catch(err => {
+  console.log(err); // eslint-disable-line no-console
+  process.exit(1);
+});
