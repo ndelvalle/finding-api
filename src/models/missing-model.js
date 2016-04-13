5468f673202223dde8a5f124ec3db3e73a2a032f
ndelvalle <nicolas.delvalle@gmail.com>
2016-04-14 00:10:09 +0200

    Implemented query generator helper at missing model

--- src/models/missing-model.js
+++ src/models/missing-model.js
@@ -1,6 +1,14 @@
 import Model from '../libraries/model';
 import Missing from '../schemas/missing-schema';
+import queryModifier from 'get-query-modifier';
+import { missing as queryGenerator } from '../helper/query-generator';
 
-class MissingModel extends Model {}
+class MissingModel extends Model {
+  find(query) {
+    const modifier = queryModifier((query));
+    return modifier(this.SchemaModel.find(queryGenerator(query)).lean())
+      .execAsync();
+  }
+}
 
 export default new MissingModel(Missing);
