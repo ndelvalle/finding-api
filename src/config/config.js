c82529e538f38c63b71cd37c452c19a7923c8e40
ndelvalle <nicolas.delvalle@gmail.com>
2016-04-18 20:41:37 +0200

    Added AWS S3 config variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 9090,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/missing',
   ENV: process.env.NODE_ENV || 'development',
   JWT: {
@@ -7,9 +7,9 @@ export default {
     EXPIRATION: process.env.JWT_EXPIRATION || 86400,
   },
   AWS: {
-    ACCESS_KEY_ID: '',
-    SECRET_ACCESS_KEY: '',
-    REGION: 'us-west-2',
-    BUCKET: 'missing-images',
+    ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || '',
+    SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || '',
+    REGION: process.env.AWS_REGION || '',
+    BUCKET: process.env.AWS_BUCKET || '',
   },
 };
