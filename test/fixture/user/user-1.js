f0998159f4a48db57d72fb061199a6c295ad3df2
ndelvalle <nicolas.delvalle@gmail.com>
2016-07-23 23:07:58 +0800

    Remove ObjectId usage and upgrade mongodb version

--- test/fixture/user/user-1.js
+++ test/fixture/user/user-1.js
@@ -1,8 +1,5 @@
-const ObjectId = require('mongodb').ObjectId;
-
 
 const User1 = {
-  _id: new ObjectId('12a3d077c143c921072e342a'),
   username: 'foobar',
   password: '$2a$10$zT9/wglteSc8mnAtdd4j.u0T8LINkBbkW1t29MUjYaEmpjHBSm4bq',
   isAdmin : true
