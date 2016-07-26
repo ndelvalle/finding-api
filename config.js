a5f52963d50dd08b52f78fb2b2b85d370467a64e
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-07-26 00:42:00 -0300

    Moving dot env to dev command

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const milieu = require('milieu');
 
 const config = milieu('api', {
--- route/root.js
+++ route/root.js
@@ -10,7 +10,8 @@ function getRoot(req, res, next) {
 
   res.send({
     name   : pkg.name,
-    version: pkg.version
+    version: pkg.version,
+    env    : process.env.NODE_ENV
   });
 }
 
