a98a02ae44aaded64d9f542adc887f3ac59a3138
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-07-26 00:59:46 -0300

    Renaming api spec and fixing config

--- test/config.js
+++ test/config.js
@@ -5,4 +5,7 @@ config.server.url = 'http://localhost:8050';
 config.mongo.url  = 'mongodb://localhost/api-test';
 config.logger     = {};
 
+config.auth0.clientID = 'aGVsbG8=';
+config.auth0.clientSecret = 'aGVsbG8=';
+
 module.exports = config;
