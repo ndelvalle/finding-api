52fce05eb3811add41d0a522e9075afcbc701a2f
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-07-26 23:10:42 -0300

    Adding remove and deleting restore

--- route/user.js
+++ route/user.js
@@ -7,9 +7,9 @@ function createUser(req, res, next) {
 
   req.auth0.management.users
     .create(req.body)
-    .then(clientRes => {
-      req.logger.verbose('Sending user to client');
-      return res.send(clientRes);
+    .then(user => {
+      req.logger.verbose('Created user with id %s', user.userId);
+      return res.send(user);
     })
     .catch(err => res.status(err.statusCode).send(err));
 }
@@ -30,7 +30,7 @@ function queryUsers(req, res, next) {
   req.auth0.management.users
     .getAll(params)
     .then(users => {
-      req.logger.verbose('Sending users to client');
+      req.logger.verbose('Found %s users', users.length);
       return res.status(200).send(users);
     })
     .catch(err => res.status(err.statusCode).send(err));
@@ -43,58 +43,38 @@ function findUserById(req, res, next) {
   req.auth0.management.users
     .get({ id })
     .then(user => {
-      req.logger.verbose('Sending user to client');
+      req.logger.verbose('Found user with id %s', id);
       return res.status(200).send(user);
     })
     .catch(err => res.status(err.statusCode).send(err));
 }
 
 function updateUserById(req, res, next) {
-  req.logger.info('Updating user with id %s', req.params.id);
-  req.model('User').update({
-    _id: req.params.id
-  }, req.body, (err, results) => {
-    if (err) { return next(err); }
+  const id = req.params.id;
+  req.logger.info('Updating user with id %s', id);
 
-    if (results.n < 1) {
-      req.logger.verbose('User not found');
-      return res.status(404).end();
-    }
-    req.logger.verbose('User updated');
-    res.status(204).end();
-  });
+  req.auth0.management.users
+    .update({ id }, req.body)
+    .then(user => {
+      req.logger.verbose('Updated user with id %s', id);
+      return res.send(user);
+    })
+    .catch(err => res.status(err.statusCode).send(err));
+
 }
 
 function removeUserById(req, res, next) {
+  // TODO: check if is needed soft remove
+  const id = req.params.id;
   req.logger.info('Removing user with id %s', req.params.id);
-  req.model('User').remove({
-    _id: req.params.id
-  }, (err, results) => {
-    if (err) { return next(err); }
 
-    if (results.nModified < 1) {
-      req.logger.verbose('User not found');
-      return res.status(404).end();
-    }
-    req.logger.verbose('User removed');
-    res.status(204).end();
-  });
-}
-
-function restoreUserById(req, res, next) {
-  req.logger.info('Restoring user with id %s', req.params.id);
-  req.model('User').restore({
-    _id: req.params.id
-  }, (err, results) => {
-    if (err) { return next(err); }
-
-    if (results.nModified < 1) {
-      req.logger.verbose('User not found');
-      return res.status(404).end();
-    }
-    req.logger.verbose('User restored');
-    res.status(204).end();
-  });
+  req.auth0.management.users
+    .delete({ id })
+    .then(user => {
+      req.logger.verbose('Removed user with id %s', id);
+      return res.send(user);
+    })
+    .catch(err => res.status(err.statusCode).send(err));
 }
 
 router.post(  '/',            createUser);
@@ -102,7 +82,6 @@ router.get(   '/',            queryUsers);
 router.get(   '/:id',         findUserById);
 router.put(   '/:id',         updateUserById);
 router.delete('/:id',         removeUserById);
-router.post(  '/restore/:id', restoreUserById);
 
 
 module.exports = router;
