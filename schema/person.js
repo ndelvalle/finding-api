91efa73ce2415dcab87779903c8ef2f8cb9acf15
Matias Tucci <matiastucci@gmail.com>
2016-07-29 16:22:31 +0800

    postalCode optional

--- schema/person.js
+++ schema/person.js
@@ -6,7 +6,7 @@ const geo = {
   loc        : { type: [Number], index: '2dsphere', required: true },
   address    : { type: String, required: true },
   city       : { type: String, required: true },
-  postalCode : { type: String, required: true },
+  postalCode : { type: String },
   countryCode: { type: String, required: true },
   country    : { type: String, required: true }
 };
