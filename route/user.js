2813c1da00dee230d075f3587cdfad662c91dee1
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-08-04 02:33:31 -0300

    Adding role and organization to user

--- route/user.js
+++ route/user.js
@@ -2,11 +2,24 @@ const Router = require('express').Router;
 const router = new Router();
 
 
+// TODO: validate if role and/or organization exist and check if will be save id or name
+
 function createUser(req, res, next) {
   req.logger.info('Creating user', req.body);
 
+  if (!req.body.role) {
+    return res.status(400).send('Missing role parameter.').end();
+  }
+
+  const user = Object.assign({ user_metadata: {} }, req.body);
+  user.user_metadata.role = req.body.role;
+  user.user_metadata.organization = req.body.organization;
+
+  delete user.role;
+  delete user.organization;
+
   req.auth0.management.users
-    .create(req.body)
+    .create(user)
     .then(user => {
       req.logger.verbose('Created user with id %s', user.userId);
       return res.status(201).send(user);
@@ -53,8 +66,18 @@ function updateUserById(req, res, next) {
   const id = req.params.id;
   req.logger.info('Updating user with id %s', id);
 
+  const user = Object.assign({ user_metadata: {} }, req.body);
+
+  if (req.body.role) {
+    user.user_metadata.role = req.body.role;
+    delete user.role;
+  }
+
+  user.user_metadata.organization = req.body.organization;
+  delete user.organization;
+
   req.auth0.management.users
-    .update({ id }, req.body)
+    .update({ id }, user)
     .then(user => {
       req.logger.verbose('Updated user with id %s', id);
       return res.status(204).send(user);
