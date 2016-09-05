ce053b53dd5b25e5e1201fb0127ab38bfc1daa26
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-09-05 12:37:16 -0300

    Updating nomad file and adding script

--- NomadFile.js
+++ NomadFile.js
@@ -1,12 +1,31 @@
-const MongoClient = require('mongodb').MongoClient;
-const config      = require('./config');
+const ObjectId     = require('mongodb').ObjectId;
+const MongoClient  = require('mongodb').MongoClient;
+const readFileSync = require('fs').readFileSync;
+const config       = require('./config');
 
 
 module.exports = function(nomad) {
+  nomad.context.ObjectId = ObjectId;
 
   nomad.driver({
     connect(cb) {
-      MongoClient.connect(config.mongo.url, (err, db) => {
+      const mongoConfig = config.mongo;
+      const mongoOpts   = {};
+
+      if (mongoConfig.sslKey && mongoConfig.sslCert && mongoConfig.sslCA) {
+
+        const subSectionName = mongoConfig.url.match(/,/) ? 'replSet' : 'server';
+        const subSection     = mongoOpts[subSectionName] = {};
+
+        subSection.ssl         = true;
+        subSection.sslValidate = mongoConfig.sslValidate;
+        subSection.sslCA       = readFileSync(mongoConfig.sslCA);
+        subSection.sslCert     = readFileSync(mongoConfig.sslCert);
+        subSection.sslKey      = readFileSync(mongoConfig.sslKey);
+        subSection.sslPass     = mongoConfig.sslPass;
+      }
+
+      MongoClient.connect(mongoConfig.url, mongoOpts, (err, db) => {
         if (err) { return cb(err); }
         this.db = db;
         cb(null, db);
