e78402ad8f6aa26889af54fd8f5b61a6727603ef
ndelvalle <nicolas.delvalle@gmail.com>
2016-09-05 18:37:37 +0700

    Add person request fixtures

---
+++ test/fixture/person-request/update-person-request-1.js
@@ -0,0 +1,12 @@
+const person1 = {
+  name       : 'Foo Bar',
+  age        : 24,
+  gender     : 'M',
+  lastSeenAt : new Date(),
+  description: { clothing: 'Blue jeans', appearance: 'short beard' },
+  contacts   : [{ name: 'John Doe', phone: '1234567', email: 'johndoe@email.com' }],
+  geo        : 'Bar'
+};
+
+
+module.exports = person1;
