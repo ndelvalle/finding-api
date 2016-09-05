51380fa4449666bf71f0bb5fba412a05ff2b1afb
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-09-05 12:45:46 -0300

    Fixing nomad collections and jwt query

--- migrations/20160814-175215-00.add-mati-user-profile.js
+++ migrations/20160814-175215-00.add-mati-user-profile.js
@@ -8,8 +8,8 @@ exports.isIgnored    = false;
 
 
 exports.up = function(db, done) {
-  db.collection('users').insertOne({
-    _id          : new ObjectId('57ad49a3cc8672aa14006c19'),
+  db.collection('userprofiles').insertOne({
+    _id          : new ObjectId('57ad49a3cc8672aa14006c00'),
     role         : new ObjectId('57a2cada0e8c0d408651e6ff'),
     organization : new ObjectId('57ad47e540ae419411780bbf'),
     auth0        : 'auth0|57979294a972791a72831c66',
@@ -20,7 +20,7 @@ exports.up = function(db, done) {
 };
 
 exports.down = function(db, done) {
-  db.collection('accounts').removeOne({
+  db.collection('userprofiles').removeOne({
     _id: new ObjectId('57ad49a3cc8672aa14006c19')
   }, (err) => done(err));
 };
