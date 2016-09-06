88a863d9e8a8f1db0e28bc7152ae845990c617c4
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-09-06 02:46:45 -0300

    Fixing person test cases

--- test/fixture/person/new-person-1.js
+++ test/fixture/person/new-person-1.js
@@ -1,14 +1,15 @@
-
 const newPerson1 = {
-  name       : 'Nicolas Del Valle',
-  age        : 24,
-  gender     : 'M',
-  lastSeenAt : new Date(),
-  isBrowsable: true,
-  isMissing  : true,
-  description: { clothing: 'Blue jeans', appearance: 'long beard' },
-  contacts   : [{ name: 'John Doe', phone: '1234567', email: 'johndoe@email.com' }],
-  photos     : [
+  _id          : '57ce56f35a6fe8ba8baf03cd',
+  organization : '57ad47e540ae419411780bbf',
+  name         : 'Nicolas Del Valle',
+  age          : 24,
+  gender       : 'M',
+  lastSeenAt   : new Date(),
+  isBrowsable  : true,
+  isMissing    : true,
+  description  : { clothing: 'Blue jeans', appearance: 'long beard' },
+  contacts     : [{ name: 'John Doe', phone: '1234567', email: 'johndoe@email.com' }],
+  photos       : [
     { order: 0, url: 'http://www.casinoefbet.com/wp-content/uploads/2015/02/bill-gates-wealthiest-person.jpg' },
     { order: 1, url: 'http://www.casinoefbet.com/wp-content/uploads/2015/02/bill-gates-wealthiest-person.jpg' }
   ],
--- test/route.person.spec.js
+++ test/route.person.spec.js
@@ -76,8 +76,6 @@ describe('Person Routes', () => {
     });
 
     it('responds with a 400 error if the body of the request does not align with the person schema', (cb) => {
-
-
       request.post('/person', { json: { foo: 'bar' } }, (err, clientRes) => {
         if (err) { return cb(err); }
 
@@ -91,7 +89,7 @@ describe('Person Routes', () => {
   describe('Query Persons Route - GET /', () => {
     beforeEach(done => connection.db.collection('persons').insertOne(person1Fixture, done));
 
-    it.only('searches for persons documents in the database', (cb) => {
+    it('searches for persons documents in the database and respons with 200 status code', (cb) => {
       request.get('/person', { json: true }, (err, clientRes) => {
         if (err) { return cb(err); }
 
@@ -101,8 +99,7 @@ describe('Person Routes', () => {
           return person;
         });
 
-        console.log(clientRes.body);
-
+        assert.equal(clientRes.statusCode, 200);
         assertContains(clientRes.body, [newPerson1Fixture]);
 
         cb(null);
@@ -111,7 +108,7 @@ describe('Person Routes', () => {
   });
 
   describe.skip('Query Persons by geolocation Route - GET /', () => {
-    beforeEach((cb) => connection.db.collection('persons').insertOne(person1Fixture, cb));
+    beforeEach(done => connection.db.collection('persons').insertOne(person1Fixture, done));
 
     it('searches for persons documents by geolocation in the database', (cb) => {
       const longitude = newPerson1Fixture.geo.loc[0];
@@ -170,16 +167,27 @@ describe('Person Routes', () => {
   });
 
   describe('Get Person Route - GET /:id', () => {
-    beforeEach( (cb) => connection.db.collection('persons').insertOne(person1Fixture, cb));
+    beforeEach(done => connection.db.collection('persons').insertOne(person1Fixture, done));
 
-    it.skip('retrieves a person document in the database', (cb) => {
+    it('retrieves a person document in the database and responds 200 status code', (cb) => {
       request.get(`person/${person1Fixture._id.toString()}`, { json: true }, (err, clientRes) => {
         if (err) { return cb(err); }
 
         clientRes.body.lastSeenAt  = new Date(clientRes.body.lastSeenAt);
         clientRes.body.isBrowsable = true;
 
+        assert.equal(clientRes.statusCode, 200);
         assertContains(clientRes.body, newPerson1Fixture);
+
+        cb(null);
+      });
+    });
+
+    it('responds with a 404 error when a person does not exist', (cb) => {
+      request.get('person/57ce56f35a6fe8ba8baf0111', { json: true }, (err, clientRes) => {
+        if (err) { return cb(err); }
+
+        assert.equal(clientRes.statusCode, 404);
 
         cb(null);
       });
@@ -208,20 +216,16 @@ describe('Person Routes', () => {
     });
 
     it('responds with a 404 error if a document does not exist with the given id', (cb) => {
-      connection.db.collection('persons').insertOne(person1Fixture, (err) => {
+      request.put('person/5d9e362ece1cf00fa05efb96', { json: updatePerson1Fixture }, (err, clientRes) => {
         if (err) { return cb(err); }
 
-        request.put('person/5d9e362ece1cf00fa05efb96', { json: updatePerson1Fixture }, (err, clientRes) => {
-          if (err) { return cb(err); }
-
-          assert.equal(clientRes.statusCode, 404);
-          cb(null);
-        });
+        assert.equal(clientRes.statusCode, 404);
+        cb(null);
       });
     });
   });
 
-  describe.skip('Remove Person Route - DELETE /:id', () => {
+  describe('Remove Person Route - DELETE /:id', () => {
     beforeEach( (cb) => connection.db.collection('persons').insertOne(person1Fixture, cb));
 
     it('removes a person document from the database', (cb) => {
@@ -248,7 +252,7 @@ describe('Person Routes', () => {
       );
     });
 
-    it.skip('restores a person document in the database', (cb) => {
+    it('restores a person document in the database', (cb) => {
       request.post(`person/restore/${person1Fixture._id.toString()}`, (err, clientRes) => {
         if (err) { return cb(err); }
 
