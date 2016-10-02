9a2057d1a23140363ad0910a07fc1daedbc063fa
ndelvalle <nicolas.delvalle@gmail.com>
2016-10-03 01:14:37 +0700

    Add disappearance field to person and person request schema

--- schema/person-request.js
+++ schema/person-request.js
@@ -4,7 +4,7 @@ const personRequestSchema = new Schema({
   name       : { type: String, required: true },
   age        : { type: Number, required: true, min: 0, max: 120 },
   gender     : { type: String, required: true, enum: 'M F'.split(' ') },
-  description: { clothing: String, appearance: String },
+  description: { clothing: String, appearance: String, disappearance: String },
   contacts   : [{ name: String, phone: String, email: String }],
   lastSeenAt : { type: Date },
   geo        : { type: String }
