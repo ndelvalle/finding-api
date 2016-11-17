2ef581fe70c70f9669ce65e13326142ffea1c4d1
Brian stanley <brianelblog@gmail.com>
2016-11-17 15:15:17 -0300

    adding geo to personRequest schema

--- schema/person-request.js
+++ schema/person-request.js
@@ -1,4 +1,19 @@
 const Schema = require('mongoose').Schema;
+const validators   = {
+  location: {
+    validator(v) { return v.length === 2; },
+    message: '{VALUE} is not a valid location'
+  }
+};
+
+const geo = {
+  loc        : { type: [Number], index: '2dsphere', required: true, validate: validators.location },
+  address    : { type: String, required: true },
+  city       : { type: String, required: true },
+  countryCode: { type: String, required: true },
+  country    : { type: String, required: true },
+  postalCode : { type: String }
+};
 
 const personRequestSchema = new Schema({
   name       : { type: String, required: true },
@@ -8,7 +23,7 @@ const personRequestSchema = new Schema({
   contacts   : [{ name: String, phone: String, email: String }],
   lastSeenAt : { type: Date },
   approved   : { type: Boolean, default: false },
-  geo        : { type: String }
+  geo
 });
 
 
