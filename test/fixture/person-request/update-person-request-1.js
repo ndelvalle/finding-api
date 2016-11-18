b6c7881b55dbf7040f2ca45a3fac15a9236c4acf
Brian stanley <brianelblog@gmail.com>
2016-11-18 12:07:49 -0300

    updating tests

--- test/fixture/person-request/update-person-request-1.js
+++ test/fixture/person-request/update-person-request-1.js
@@ -5,7 +5,7 @@ const personRequest1 = {
   lastSeenAt : new Date(),
   description: { clothing: 'Blue jeans', appearance: 'short beard' },
   contacts   : [{ name: 'John Doe', phone: '1234567', email: 'johndoe@email.com' }],
-  geo        : 'Bar'
+  geo: 'Bar'
 };
 
 
