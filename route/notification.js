17c2dd6642325d7a4082fb71f0aa152e7f1bc694
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-12-06 02:02:39 -0300

    Refactoring notif route

--- route/notification.js
+++ route/notification.js
@@ -5,9 +5,13 @@ const request  = require('request');
 
 function getNotifications(req, res, next) {
   req.logger.info('Querying notifications', req.query);
+
   request.get(`${req.config.notificationsApi.url}bundle/`, (err, clientRes, body) => {
     if (err) { return next(err); }
-    if (clientRes.statusCode !== 200) { return res.status(clientRes.statusCode).send(body).end(); }
+    if (clientRes.statusCode !== 200) {
+      return res.status(clientRes.statusCode).send(body).end();
+    }
+
     req.logger.verbose('Sending notifications to user client');
     res.status(200).send(body);
   });
@@ -15,34 +19,30 @@ function getNotifications(req, res, next) {
 
 function createNotification(req, res, next) {
   req.logger.info('Creating a notification', req.body);
-  const notification = {
-    name      : req.body.name,
-    title     : req.body.title,
-    body      : req.body.body,
-    type      : req.body.type,
-    status    : req.body.status,
-    sentAt    : req.body.sentAt,
-    createdBy : req.body.createdBy
-  };
+
+  const notification = req.body;
+
   req.auth0.management.users.getAll()
   .then((users) => {
     notification.users = users.map( a => a.user_id );
     return notification;
   })
   .then((notification) => {
-    request.post(`${req.config.notificationsApi.url}bundle/`, { json: notification }, (err, clientRes, body) => {
-      if (err) { return next(err); }
-      if (clientRes.statusCode !== 200) {
-        return res.status(clientRes.statusCode).send(body).end();
-      }
-      req.logger.verbose('Sending created notification to client');
+    request.post(`${req.config.notificationsApi.url}bundle/`,
+      { json: notification }, (err, clientRes, body) => {
+        if (err) { return next(err); }
+        if (clientRes.statusCode !== 200) {
+          return res.status(clientRes.statusCode).send(body).end();
+        }
 
-      res.status(200).send(body);
-    });
+        req.logger.verbose('Sending created notification to client');
+
+        res.status(200).send(body);
+      });
   });
 }
 
-router.get('/notification-set', getNotifications);
-router.post(  '/',    createNotification);
+router.get(  '/notification-set', getNotifications);
+router.post( '/',                 createNotification);
 
 module.exports = router;
