f5ea02bf585d9e2580e69b6d7c5ac4f0d831b047
ndelvalle <nicolas.delvalle@gmail.com>
2017-02-12 23:53:06 -0300

    Use a fixed node version and fix auth0 config vars

--- test/config.js
+++ test/config.js
@@ -7,7 +7,7 @@ config.server.port = 8050;
 config.mongo.url  = 'mongodb://localhost/api-test';
 config.logger     = {};
 
-config.auth0.clientID     = 'aaa';
+config.auth0.clientId     = 'aaa';
 config.auth0.clientSecret = 'bbb';
 config.auth0.token        = 'ccc';
 
