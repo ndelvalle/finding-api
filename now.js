b5e5ce3624ec3874fb332ff2dffdd3c27b72dd62
ndelvalle <nicolas.delvalle@gmail.com>
2017-04-20 02:19:39 -0400

    Improve code style

--- now.js
+++ now.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs        = require('fs');
 const nowConfig = require('./now.json');
+
 nowConfig.alias = process.env.ALIAS || 'api.find.earth';
 fs.writeFileSync('now.json', JSON.stringify(nowConfig, null, 2), 'utf8');
